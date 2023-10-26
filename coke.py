39674168db01635a3d34e463006c3682aa64d36a
Usman Virk <usmanvirk11@gmail.com>
2023-10-26 22:50:01 +0200

    Final

--- coke.py
+++ coke.py
@@ -1,6 +1,3 @@
-                                                        # total_amount = 50
-                                                        # print(f"amount due: {total_amount}")
-
 amount_balance = 50
 
 while amount_balance > 0:
@@ -8,14 +5,11 @@ while amount_balance > 0:
 
     coin = int(input("Insert Coin:"))
 
-                                                        # if coin == 5 and coin == 10 and coin == 25:
-                                                            # amount_balance = amount_balance - coin
 
 
     if coin == 5 or coin == 10 or coin == 25:
         amount_balance = amount_balance - coin
-                                                        # amount_balance = abs(amount_balance - coin)
-                                                        # print("Change Owned : ", amount_balance)
+
 
 change_owned = abs(amount_balance)
 print("Change Owed:",change_owned)
